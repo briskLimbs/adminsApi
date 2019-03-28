ddd0e702aba6f29d769d73cd33e7f35bf1ef6bcd
Saqib Razzaq <saqibrzzaq@gmail.com>
2019-03-28 13:03:45 +0500

    Initial code calls added

--- index.php
+++ index.php
@@ -2,29 +2,47 @@
 
 require 'vendor/autoload.php';
 require 'functions.php';
-// require 'database.php';
+require 'database.php';
 
 $app = new Slim\App();
 
 $app->get('/versions', function ($request, $response, $args) {
-  db("All versions etc");
+  displayMessage("All versions etc");
 });
 
 $app->get('/versions/{version}', function ($request, $response, $args) {
-  db("Single version " . $args['version']);
+  displayMessage("Single version " . $args['version']);
 });
 
 $app->get('/addons', function ($request, $response, $args) {
 	pr($request);
-  db("All addons etc");
+  displayMessage("All addons etc");
 });
 
 $app->get('/addons/{name}', function ($request, $response, $args) {
-  db("Single addon " . $args['name']);
+  displayMessage("Single addon " . $args['name']);
 });
 
 $app->get('/addons/search/{keyword}', function ($request, $response, $args) {
-  db("Search addon " . $args['keyword']);
+  displayMessage("Search addon " . $args['keyword']);
 });
+
+$app->get('/skins', function ($request, $response, $args) {
+	pr($request);
+  displayMessage("All skins etc");
+});
+
+$app->get('/skins/{name}', function ($request, $response, $args) {
+  displayMessage("Single skin " . $args['name']);
+});
+
+$app->get('/skins/search/{keyword}', function ($request, $response, $args) {
+  displayMessage("Search skin " . $args['keyword']);
+});
+
+$app->get('/news', function ($request, $response, $args) {
+  displayMessage("Get news ");
+});
+
 
 $app->run();
